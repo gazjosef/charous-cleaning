459665b2e4e0fe63f798876c2c18232a86463b03
Gareth Hind <garethjhind@gmail.com>
2022-12-15 14:30:54 +1100

    Added TailwindCSS

---
+++ components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <h1>Header</h1>
+    </div>
+    <nav class="header__nav">
+      <ul>
+        <li>Home</li>
+        <li>Services</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+.header {
+  background: red;
+
+  display: flex;
+  justify-content: space-between;
+}
+
+ul {
+  display: flex;
+}
+
+li {
+  padding-left: 10px;
+  padding-right: 10px;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,21 @@
 <template>
-  <Tutorial/>
+  <div>
+    <Test />
+    <Header />
+    <Tutorial />
+  </div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'IndexPage'
-})
+  name: "IndexPage",
+});
 </script>
+
+<styles>
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
+</styles>
