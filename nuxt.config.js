a2bed4d496766bbbfdf3af731c20fa64e004d7d9
Gareth Hind <garethjhind@gmail.com>
2022-12-15 22:32:24 +1100

    Updated Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: "charous-cleaning",
+    title: "Charous Cleaning | Pool Repair",
     htmlAttrs: {
       lang: "en",
     },
