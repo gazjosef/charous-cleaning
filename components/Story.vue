a2b9dd55630a9174e8d07a4d21adbce44555684a
Gareth Hind <garethjhind@gmail.com>
2022-12-20 22:42:46 +1100

    Updated Story.vue

--- components/Story.vue
+++ components/Story.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-bermuda min-h-screen grid place-content-center">
+  <div class="bg-bermuda min-h-screen | grid place-content-center">
     <h1>Story</h1>
   </div>
 </template>
