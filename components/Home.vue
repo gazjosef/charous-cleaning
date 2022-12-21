48b29b2210ae8ae12dcadcdc04c6e492c79b4119
Gareth Hind <garethjhind@gmail.com>
2022-12-22 01:06:19 +1100

    Updated

--- components/Home.vue
+++ components/Home.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="bg-indigoBlue min-h-screen grid place-content-center text-center">
-    <h1 class="mb-2 text-5xl">Mountain</h1>
+    <div class="min-h-[50%] flex flex-col">
+      <h1 class="mb-2 text-5xl">Mountain</h1>
 
-    <p class="mb-2">WELCOME TO MOUNTAIN WEBSITE. AGENCY IN BERLIN.</p>
+      <p class="mb-2">WELCOME TO MOUNTAIN WEBSITE. AGENCY IN BERLIN.</p>
 
-    <button class="border border-white py-4 px-4 rounded max-w-min font-bold">
-      Discover
-    </button>
+      <button class="border border-white py-4 px-4 rounded max-w-min font-bold">
+        Discover
+      </button>
+    </div>
   </div>
 </template>
 
--- components/Story.vue
+++ components/Story.vue
@@ -1,6 +1,20 @@
 <template>
-  <div class="bg-bermuda min-h-screen | grid place-content-center">
-    <h1>Story</h1>
+  <div
+    class="bg-bermuda min-h-screen | grid place-content-center | text-center"
+  >
+    <h1 class="mb-4 font-bold leading-tight text-4xl">Story</h1>
+    <div class="grid md:grid-cols-2">
+      <div>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. In at enim
+          ut, explicabo sequi eligendi.
+        </p>
+      </div>
+      <div>
+        <img src="" alt="" />
+        IMAGE
+      </div>
+    </div>
   </div>
 </template>
 
