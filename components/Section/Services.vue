07ce7585b4eed0a663f7c7c4f566405d1b2883d4
Gareth Hind <garethjhind@gmail.com>
2022-12-25 19:24:09 +1100

    Updated

--- components/Section/Services.vue
+++ components/Section/Services.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="min-h-screen grid place-content-center text-center">
     <div class="container">
-      <h1 class="mb-4 text-center text-4xl font-bold">Services</h1>
+      <h1 class="primary-heading | mb-4">Services</h1>
       <div class="grid md:grid-cols-3 gap-4">
-        <UICard />
-        <UICard />
-        <UICard />
+        <UICard
+          title="helloWorld"
+          excerpt="Some quick example text to build on the card title and make up the bulk
+        of the card's content."
+        />
+        <UICard
+          title="helloWorld2"
+          excerpt="Some quick example text to build on the card title and make up the bulk
+        of the card's content."
+        />
+        <UICard
+          title="helloWorld3"
+          excerpt="Some quick example text to build on the card title and make up the bulk
+        of the card's content."
+        />
       </div>
     </div>
   </div>
--- components/UI/Button.vue
+++ components/UI/Button.vue
@@ -1,5 +1,10 @@
 <template>
-  <div></div>
+  <button
+    type="button"
+    class="inline-block px-6 py-2.5 bg-blue-600 font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+  >
+    Button
+  </button>
 </template>
 
 <script>
