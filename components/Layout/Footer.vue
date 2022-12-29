8b19f6e380664662666917244510f8b44c8f7554
Gareth Hind <garethjhind@gmail.com>
2022-12-29 22:20:00 +1100

    Updated Footer

--- components/Layout/Footer.vue
+++ components/Layout/Footer.vue
@@ -1,5 +1,6 @@
 <template>
   <footer class="text-center lg:text-left bg-gray-100 text-gray-600">
+    <!-- SOCIAL MEDIA LINKS -->
     <div
       class="flex justify-center items-center lg:justify-between p-6 border-b border-gray-300"
     >
@@ -111,6 +112,7 @@
         </a>
       </div>
     </div>
+    <!-- COLUMNS -->
     <div class="mx-6 py-10 text-center md:text-left">
       <div class="grid grid-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
         <div class=""></div>
@@ -233,13 +235,9 @@
         </div>
       </div>
     </div>
+    <!-- COPYRIGHT -->
     <div class="text-center p-6 bg-gray-200">
-      <span>© 2021 Copyright:</span>
-      <a
-        class="text-gray-600 font-semibold"
-        href="https://tailwind-elements.com/"
-        >Tailwind Elements</a
-      >
+      <span>© 2022 Copyright</span>
     </div>
   </footer>
 </template>
