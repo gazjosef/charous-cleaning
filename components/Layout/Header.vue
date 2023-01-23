f876c44ac844949031abcc21dea1854b917393fc
Gareth Hind <garethjhind@gmail.com>
2023-01-23 19:08:08 +1100

    Added Notification Bar

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="relative">
+    <SectionNotificationBar />
     <LayoutNavbar />
   </div>
 </template>
---
+++ components/Section/NotificationBar.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="bg-black | text-white">Notification Bar</div>
+</template>
+
+<script>
+export default {
+  name: "SectionNotificationBar",
+};
+</script>
+
+<style lang="css"></style>
