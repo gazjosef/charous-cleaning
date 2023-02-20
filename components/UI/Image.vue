b31a69772df6af0aab200985f2a4428cf7342f70
Gareth Hind <garethjhind@gmail.com>
2023-02-21 09:51:07 +1100

    Revert "Added Sharp Package"
    This reverts commit 6e1425f2c5e6c73c4d08b65712b9c7a0a48f036d.

--- components/UI/Image.vue
+++ components/UI/Image.vue
@@ -7,24 +7,12 @@
 </template>
 
 <script>
-// import sharp from "sharp";
-
 export default {
   props: {
     image: String,
     alt: String,
   },
   name: "UIImage",
-  // computed: {
-  //   imageUrl() {
-  //     const path = `@/assets/images/${this.image}.jpg`;
-  //     const buffer = sharp(path)
-  //       .resize({ width: 500 })
-  //       .jpeg({ quality: 80 })
-  //       .toBuffer();
-  //     return `data:image/jpeg;base64,${buffer.toString("base64")}`;
-  //   },
-  // },
 };
 </script>
 
