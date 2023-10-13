42ddf199ec62a70a633d87e6863891f6b4f5710c
Gareth Hind <garethjhind@gmail.com>
2023-10-13 19:08:16 +1100

    Added pages

---
+++ pages/gallery.vue
@@ -0,0 +1,71 @@
+<template>
+  <div class="min-h-screen mx-auto py-20 | grid place-content-center">
+    <div class="container">
+      <UIPrimaryHeading title="Follow Us @Charous Cleaning" />
+      <div class="grid md:grid-cols-3 gap-4">
+        <UIGalleryImage
+          image="pool1"
+          title="pool1"
+          excerpt="Some quick example text to build on the card title and make up the bulk
+            of the card's content."
+        />
+        <UIGalleryImage
+          image="pool2"
+          title="pool2"
+          excerpt="Some quick example text to build on the card title and make up the bulk
+            of the card's content."
+        />
+        <UIGalleryImage
+          image="pool3"
+          title="pool3"
+          excerpt="Some quick example text to build on the card title and make up the bulk
+            of the card's content."
+        />
+        <UIGalleryImage
+          image="pool4"
+          title="pool4"
+          excerpt="Some quick example text to build on the card title and make up the bulk
+            of the card's content."
+        />
+        <UIGalleryImage
+          image="pool5"
+          title="pool5"
+          excerpt="Some quick example text to build on the card title and make up the bulk
+            of the card's content."
+        />
+        <UIGalleryImage
+          image="pool6"
+          title="pool6"
+          excerpt="Some quick example text to build on the card title and make up the bulk
+            of the card's content."
+        />
+        <UIGalleryImage
+          image="pool7"
+          title="pool7"
+          excerpt="Some quick example text to build on the card title and make up the bulk
+            of the card's content."
+        />
+        <UIGalleryImage
+          image="pool8"
+          title="pool8"
+          excerpt="Some quick example text to build on the card title and make up the bulk
+            of the card's content."
+        />
+        <UIGalleryImage
+          image="pool9"
+          title="pool9"
+          excerpt="Some quick example text to build on the card title and make up the bulk
+            of the card's content."
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SectionGallery",
+};
+</script>
+
+<style lang="scss" scoped></style>
